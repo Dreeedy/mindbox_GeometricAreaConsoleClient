2939ef6b0fbbf20b19f3b89704c5f4b7f39dda03
Dreeedy <dreeedyone@gmail.com>
2021-11-16 17:06:11 +0300

    Add project files.

---
+++ mindbox_GeometricAreaConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mindbox_GeometricAreaConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
